532ed98023d62d90210782f59f9546f295169c88
margarita0912 <2202199@ucc.edu.ar>
2023-06-02 21:11:32 -0300

    hola

---
+++ ClionProyectoProgramacion/cmake-build-debug/CMakeFiles/ClionProyectoProgramacion.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/marga/PROYECTOPROGRAMACION2/Proyecto/ClionProyectoProgramacion/Cliente.cpp" "CMakeFiles/ClionProyectoProgramacion.dir/Cliente.cpp.obj" "gcc" "CMakeFiles/ClionProyectoProgramacion.dir/Cliente.cpp.obj.d"
+  "C:/Users/marga/PROYECTOPROGRAMACION2/Proyecto/ClionProyectoProgramacion/Transaccion.cpp" "CMakeFiles/ClionProyectoProgramacion.dir/Transaccion.cpp.obj" "gcc" "CMakeFiles/ClionProyectoProgramacion.dir/Transaccion.cpp.obj.d"
+  "C:/Users/marga/PROYECTOPROGRAMACION2/Proyecto/ClionProyectoProgramacion/main.cpp" "CMakeFiles/ClionProyectoProgramacion.dir/main.cpp.obj" "gcc" "CMakeFiles/ClionProyectoProgramacion.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ClionProyectoProgramacion/cmake-build-debug/CMakeFiles/ClionProyectoProgramacion.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClionProyectoProgramacion.dir/Cliente.cpp.obj"
+  "CMakeFiles/ClionProyectoProgramacion.dir/Cliente.cpp.obj.d"
+  "CMakeFiles/ClionProyectoProgramacion.dir/Transaccion.cpp.obj"
+  "CMakeFiles/ClionProyectoProgramacion.dir/Transaccion.cpp.obj.d"
+  "CMakeFiles/ClionProyectoProgramacion.dir/main.cpp.obj"
+  "CMakeFiles/ClionProyectoProgramacion.dir/main.cpp.obj.d"
+  "ClionProyectoProgramacion.exe"
+  "ClionProyectoProgramacion.exe.manifest"
+  "ClionProyectoProgramacion.pdb"
+  "libClionProyectoProgramacion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClionProyectoProgramacion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
